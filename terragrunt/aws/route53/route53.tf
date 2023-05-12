2b0900dca6d62c94de4ea5b430a40e221a02b190
Pat Heard <patrick.heard@cds-snc.ca>
2023-05-12 08:15:49 -0400

    feat: add Netlify CNAME records

--- terragrunt/aws/route53/route53.tf
+++ terragrunt/aws/route53/route53.tf
@@ -15,3 +15,23 @@ resource "aws_route53_zone" "domain_fr" {
     Terraform  = true
   }
 }
+
+resource "aws_route53_record" "domain_en_CNAME" {
+  zone_id = aws_route53_zone.domain_en.zone_id
+  name    = var.domain_en
+  type    = "CNAME"
+  records = [
+    "cds-design-snc.netlify.app"
+  ]
+  ttl = "300"
+}
+
+resource "aws_route53_record" "domain_fr_CNAME" {
+  zone_id = aws_route53_zone.domain_fr.zone_id
+  name    = var.domain_fr
+  type    = "CNAME"
+  records = [
+    "cds-design-snc.netlify.app"
+  ]
+  ttl = "300"
+}
