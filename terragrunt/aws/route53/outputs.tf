4e1581e893e79ea59e2544f6e6b0d63e08f89db8
Pat Heard <patrick.heard@cds-snc.ca>
2023-10-12 12:23:51 +0000

    chore: preserve original output to remove TF plan noise

--- terragrunt/aws/route53/outputs.tf
+++ terragrunt/aws/route53/outputs.tf
@@ -1,3 +1,8 @@
+output "hosted_zone_id" {
+  description = "Route53 hosted zone ID that will hold the CDN DNS records.  Once the new `hosted_zone_id_cdn` output has been created, this can be removed and the `cdn` module can be updated."
+  value       = aws_route53_zone.content_delivery_network.zone_id
+}
+
 output "hosted_zone_id_cdn" {
   description = "Route53 hosted zone ID that will hold the CDN DNS records"
   value       = aws_route53_zone.content_delivery_network.zone_id
