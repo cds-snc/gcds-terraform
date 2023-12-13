16dc91ada016a4c31eb767eaf554decfebaa847c
Daine Trinidad <daine.trinidad@cds-snc.ca>
2023-12-13 10:20:35 -0800

    fix: move CDN NS records to website_en

--- terragrunt/aws/route53/route53.tf
+++ terragrunt/aws/route53/route53.tf
@@ -24,3 +24,18 @@ resource "aws_route53_zone" "website_fr" {
     Terraform  = true
   }
 }
+
+resource "aws_route53_record" "cdn_NS" {
+  zone_id = aws_route53_zone.website_en.zone_id
+  name    = var.domain_cdn
+
+  type    = "NS"
+
+  records = [
+    "ns-1114.awsdns-11.org",
+    "ns-830.awsdns-39.net",
+    "ns-2010.awsdns-59.co.uk",
+    "ns-111.awsdns-13.com"
+  ]
+  ttl = "300"
+}
