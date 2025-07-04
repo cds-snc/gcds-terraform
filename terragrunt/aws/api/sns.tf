d3095ce2e493602673bcdfc7e15d19e1b1213682
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2025-07-04 16:10:46 -0700

    Formatting and adding checkov skip checks

--- terragrunt/aws/api/sns.tf
+++ terragrunt/aws/api/sns.tf
@@ -2,7 +2,7 @@
 # SNS: topic & subscription
 #
 resource "aws_sns_topic" "cloudwatch_warning" {
-  name = "gc-design-system-cloudwatch-alarms-warning"
+  name              = "gc-design-system-cloudwatch-alarms-warning"
   kms_master_key_id = aws_kms_key.sns_cloudwatch.id
 
   tags = {
@@ -20,12 +20,15 @@ resource "aws_sns_topic_subscription" "alert_warning" {
 # KMS Key for SNS CloudWatch topic
 # This key is used to encrypt messages sent to the SNS topic for CloudWatch alarms.
 resource "aws_kms_key" "sns_cloudwatch" {
+  # checkov:skip=CKV_AWS_7: key rotation not required for CloudWatch SNS topic's messages
   description = "KMS key for CloudWatch SNS topic"
   policy      = data.aws_iam_policy_document.sns_cloudwatch.json
 }
 
 # IAM policy document for the KMS key used by SNS CloudWatch topic
 data "aws_iam_policy_document" "sns_cloudwatch" {
+  # checkov:skip=CKV_AWS_109: `resources = ["*"]` identifies the KMS key to which the key policy is attached
+  # checkov:skip=CKV_AWS_111: `resources = ["*"]` identifies the KMS key to which the key policy is attached
   statement {
     effect    = "Allow"
     resources = ["*"]
