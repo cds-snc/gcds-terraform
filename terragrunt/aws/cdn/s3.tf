8764c5200547f628147b1b69fc2f3cc377c41282
Pat Heard <patrick.heard@cds-snc.ca>
2025-08-06 15:20:50 -0400

    fix: enable log bucket versioning (#80)
    Update the CloudFront access log bucket to enable versioning.  This is
    required to setup replication rules.
    
    Additionally this refactors the Terraform to move the log bucket code into
    the s3.tf file for consistency.

--- terragrunt/aws/cdn/s3.tf
+++ terragrunt/aws/cdn/s3.tf
@@ -36,6 +36,66 @@ data "aws_iam_policy_document" "cloudfront_get_object" {
   }
 }
 
+# Bucket to store cloudfront logs
+module "cloudfront_logs" {
+  source            = "github.com/cds-snc/terraform-modules//S3?ref=v9.4.4"
+  bucket_name       = "${var.product_name}-${var.env}-cdn-logs"
+  billing_tag_value = var.billing_code
+
+  versioning = {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_replication_configuration" "s3_replicate_data_lake" {
+  role   = aws_iam_role.s3_replicate_data_lake.arn
+  bucket = module.cloudfront_logs.s3_bucket_id
+
+  rule {
+    id     = "send-to-platform-data-lake"
+    status = var.env == "production" ? "Enabled" : "Disabled"
+
+    destination {
+      bucket = var.platform_data_lake_raw_s3_bucket_arn
+    }
+  }
+}
+
+# Bucket policy to allow CloudFront to write logs
+resource "aws_s3_bucket_policy" "cloudfront_logs_policy" {
+  bucket = module.cloudfront_logs.s3_bucket_id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action   = "s3:PutObject",
+        Resource = "arn:aws:s3:::${module.cloudfront_logs.s3_bucket_id}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
+
+}
+
+# Set the bucket ownership controls of the log bucket to ensure that CloudFront can write logs
+resource "aws_s3_bucket_ownership_controls" "cloudfront_logs_ownership_controls" {
+  bucket = module.cloudfront_logs.s3_bucket_id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [module.cloudfront_logs]
+}
+
 #
 # Replicate the data to the Platform Data Lake
 #
