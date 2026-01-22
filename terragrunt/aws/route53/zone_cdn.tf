b40d75c7a807d61bc2c98ae653641a49176b4365
Daine Trinidad <daine.trinidad@cds-snc.ca>
2026-01-22 14:10:33 -0800

    refactor: add new vars, update vars being used

--- terragrunt/aws/route53/zone_cdn.tf
+++ terragrunt/aws/route53/zone_cdn.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "content_delivery_network" {
-  name = var.domain_cdn
+  name = var.alpha_domain_cdn
 
   tags = {
     CostCentre = var.billing_code
@@ -9,7 +9,7 @@ resource "aws_route53_zone" "content_delivery_network" {
 
 resource "aws_route53_record" "cdn_NS" {
   zone_id = aws_route53_zone.website_en.zone_id
-  name    = var.domain_cdn
+  name    = var.alpha_domain_cdn
 
   type = "NS"
 
--- terragrunt/aws/route53/zone_website_en.tf
+++ terragrunt/aws/route53/zone_website_en.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "website_en" {
-  name = var.domain_website_en
+  name = var.alpha_domain_website_en
 
   tags = {
     CostCentre = var.billing_code
