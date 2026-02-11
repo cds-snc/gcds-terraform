b595ab750fec2754c94fa7fcd6e8272f1e882fe5
Daine Trinidad <daine.trinidad@cds-snc.ca>
2026-02-11 15:51:34 -0800

    feat: add canada.ca domains for CDN and update redirects to use them (#95)
    * chore: update rewrites
    
    * chore: add ca_cdn (same access to s3 cdn bucket)

--- terragrunt/aws/cdn/cloudfront.tf
+++ terragrunt/aws/cdn/cloudfront.tf
@@ -51,3 +51,57 @@ resource "aws_cloudfront_distribution" "cdn" {
     Terraform  = true
   }
 }
+
+resource "aws_cloudfront_origin_access_identity" "ca_cdn" {
+  comment = "CloudFront CDN for CA ${var.product_name}-${var.env}"
+}
+
+resource "aws_cloudfront_distribution" "ca_cdn" {
+  enabled     = true
+  aliases     = [var.ca_domain_cdn]
+  price_class = "PriceClass_All"
+
+  origin {
+    domain_name = module.cdn_origin.s3_bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.ca_cdn.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    cache_policy_id            = local.cloudfront_cache_policy_optimized
+    origin_request_policy_id   = local.cloudfront_origin_request_policy_cors_s3origin
+    response_headers_policy_id = local.cloudfront_response_headers_policy_cors_preflight
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  logging_config {
+    bucket = module.cloudfront_logs.s3_bucket_domain_name
+    prefix = "platform/gc-design-system/cloudfront-logs/"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.cdn_certificate_validation.certificate_arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method       = "sni-only"
+  }
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
