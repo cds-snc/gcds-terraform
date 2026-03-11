54dbb04a381dab4432672f5b79d5cb2a23fd1c7e
Daine Trinidad <daine.trinidad@cds-snc.ca>
2026-03-10 20:04:14 -0700

    chore: add amplify_waf.tf

---
+++ terragrunt/aws/website/amplify_waf.tf
@@ -0,0 +1,485 @@
+# WAF ACL for Design System Documentation (EN)
+resource "aws_wafv2_web_acl" "amplify_en" {
+  provider = aws.us-east-1
+  name     = "waf-${var.product_name}-amplify-en-${var.env}"
+  scope    = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  # AWS Managed Rules - Common Rule Set (common vulnerabilities and bad bot protection)
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 0
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-en-common-rules"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Rate Limiting Rule (only POST to /api/submission/)
+  rule {
+    name     = "RateLimitRule"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 1000
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          and_statement {
+            statement {
+              regex_match_statement {
+                field_to_match {
+                  uri_path {}
+                }
+                regex_string = "^/api/submission/$"
+                text_transformation {
+                  priority = 0
+                  type     = "LOWERCASE"
+                }
+              }
+            }
+
+            statement {
+              regex_match_statement {
+                field_to_match {
+                  method {}
+                }
+                regex_string = "^post$"
+                text_transformation {
+                  priority = 0
+                  type     = "LOWERCASE"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-en-rate-limit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Block large requests to prevent abuse and DoS attacks
+  rule {
+    name     = "BlockLargeRequests"
+    priority = 2
+
+    action {
+      block {}
+    }
+
+    statement {
+      size_constraint_statement {
+        field_to_match {
+          body {
+            oversize_handling = "MATCH"
+          }
+        }
+        comparison_operator = "GT"
+        size                = 20480 # 20 KB
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-en-block-large-requests"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  # Only allow POST on /api/submission/
+  rule {
+    name     = "SubmissionMethodRestriction"
+    priority = 3
+
+    action {
+      block {}
+    }
+
+    statement {
+      and_statement {
+        statement {
+          regex_match_statement {
+            regex_string = "^/api/submission/$"
+            field_to_match {
+              uri_path {}
+            }
+            text_transformation {
+              priority = 0
+              type     = "LOWERCASE"
+            }
+          }
+        }
+
+        statement {
+          not_statement {
+            statement {
+              byte_match_statement {
+                positional_constraint = "EXACTLY"
+                search_string         = "POST"
+                field_to_match {
+                  method {}
+                }
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-en-submission-method-restriction"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # API Endpoint Protection - Whitelist known good paths (/api/submission/) and block everything else under /api/
+  rule {
+    name     = "APIEndpointProtection"
+    priority = 4
+
+    action {
+      block {
+        custom_response {
+          response_code = 403
+        }
+      }
+    }
+
+    statement {
+      and_statement {
+        statement {
+          byte_match_statement {
+            positional_constraint = "STARTS_WITH"
+            field_to_match {
+              uri_path {}
+            }
+            search_string = "/api/"
+            text_transformation {
+              priority = 0
+              type     = "LOWERCASE"
+            }
+          }
+        }
+
+        statement {
+          not_statement {
+            statement {
+              regex_match_statement {
+                regex_string = "^/api/submission/$"
+                field_to_match {
+                  uri_path {}
+                }
+                text_transformation {
+                  priority = 0
+                  type     = "LOWERCASE"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "APIEndpointProtection"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.product_name}-amplify-en"
+    sampled_requests_enabled   = true
+  }
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+
+# WAF ACL for Design System Documentation (FR)
+resource "aws_wafv2_web_acl" "amplify_fr" {
+  provider = aws.us-east-1
+  name     = "waf-${var.product_name}-amplify-fr-${var.env}"
+  scope    = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  # AWS Managed Rules - Common Rule Set
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 0
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-fr-common-rules"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Rate Limiting Rule (only POST to /api/submission/)
+  rule {
+    name     = "RateLimitRule"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 1000
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          and_statement {
+            statement {
+              regex_match_statement {
+                field_to_match {
+                  uri_path {}
+                }
+                regex_string = "^/api/submission/$"
+                text_transformation {
+                  priority = 0
+                  type     = "LOWERCASE"
+                }
+              }
+            }
+
+            statement {
+              regex_match_statement {
+                field_to_match {
+                  method {}
+                }
+                regex_string = "^post$"
+                text_transformation {
+                  priority = 0
+                  type     = "LOWERCASE"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-fr-rate-limit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Block large requests to prevent abuse and DoS attacks
+  rule {
+    name     = "BlockLargeRequests"
+    priority = 2
+
+    action {
+      block {}
+    }
+
+    statement {
+      size_constraint_statement {
+        field_to_match {
+          body {
+            oversize_handling = "MATCH"
+          }
+        }
+        comparison_operator = "GT"
+        size                = 20480 # 20 KB
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-fr-block-large-requests"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Only allow POST on /api/submission/
+  rule {
+    name     = "SubmissionMethodRestriction"
+    priority = 3
+
+    action {
+      block {}
+    }
+
+    statement {
+      and_statement {
+        statement {
+          regex_match_statement {
+            regex_string = "^/api/submission/$"
+            field_to_match {
+              uri_path {}
+            }
+            text_transformation {
+              priority = 0
+              type     = "LOWERCASE"
+            }
+          }
+        }
+
+        statement {
+          not_statement {
+            statement {
+              byte_match_statement {
+                positional_constraint = "EXACTLY"
+                search_string         = "POST"
+                field_to_match {
+                  method {}
+                }
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-fr-submission-method-restriction"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # API Endpoint Protection - Whitelist known good paths (/api/submission/) and block everything else under /api/
+  rule {
+    name     = "APIEndpointProtection"
+    priority = 4
+
+    action {
+      block {
+        custom_response {
+          response_code = 403
+        }
+      }
+    }
+
+    statement {
+      and_statement {
+        statement {
+          byte_match_statement {
+            positional_constraint = "STARTS_WITH"
+            field_to_match {
+              uri_path {}
+            }
+            search_string = "/api/"
+            text_transformation {
+              priority = 0
+              type     = "LOWERCASE"
+            }
+          }
+        }
+
+        statement {
+          not_statement {
+            statement {
+              regex_match_statement {
+                regex_string = "^/api/submission/$"
+                field_to_match {
+                  uri_path {}
+                }
+                text_transformation {
+                  priority = 0
+                  type     = "LOWERCASE"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.product_name}-amplify-fr-api-protection"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.product_name}-amplify-fr"
+    sampled_requests_enabled   = true
+  }
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+
+# Associate WAF ACL with Amplify EN app
+resource "aws_wafv2_web_acl_association" "amplify_en" {
+  provider    = aws.us-east-1
+  resource_arn = aws_amplify_app.design_system_docs_en.arn
+  web_acl_arn = aws_wafv2_web_acl.amplify_en.arn
+}
+
+# Associate WAF ACL with Amplify FR app
+resource "aws_wafv2_web_acl_association" "amplify_fr" {
+  provider    = aws.us-east-1
+  resource_arn = aws_amplify_app.design_system_docs_fr.arn
+  web_acl_arn = aws_wafv2_web_acl.amplify_fr.arn
+}
