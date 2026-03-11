662f1cd7ddece0ffc33b904430d5104479baf7e3
Daine Trinidad <daine.trinidad@cds-snc.ca>
2026-03-11 09:46:37 -0700

    chore: update acl with suggestions

--- terragrunt/aws/website/amplify_waf.tf
+++ terragrunt/aws/website/amplify_waf.tf
@@ -1,7 +1,7 @@
 # WAF ACL for Design System Documentation (EN)
-resource "aws_wafv2_web_acl" "amplify_en" {
+resource "aws_wafv2_web_acl" "amplify_docs" {
   provider = aws.us-east-1
-  name     = "waf-${var.product_name}-amplify-en-${var.env}"
+  name     = "waf-${var.product_name}-amplify-docs-${var.env}"
   scope    = "CLOUDFRONT"
 
   default_action {
@@ -26,7 +26,7 @@ resource "aws_wafv2_web_acl" "amplify_en" {
 
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-en-common-rules"
+      metric_name                = "${var.product_name}-amplify-docs-common-rules"
       sampled_requests_enabled   = true
     }
   }
@@ -49,12 +49,12 @@ resource "aws_wafv2_web_acl" "amplify_en" {
 
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-en-known-bad-inputs"
+      metric_name                = "${var.product_name}-amplify-docs-known-bad-inputs"
       sampled_requests_enabled   = true
     }
   }
 
-  # Rate Limiting Rule (only POST to /api/submission/)
+  # Rate Limiting Rule (only POST to /api/submission)
   rule {
     name     = "RateLimitRule"
     priority = 2
@@ -65,7 +65,7 @@ resource "aws_wafv2_web_acl" "amplify_en" {
 
     statement {
       rate_based_statement {
-        limit              = 1000
+        limit              = 100
         aggregate_key_type = "IP"
 
         scope_down_statement {
@@ -75,7 +75,7 @@ resource "aws_wafv2_web_acl" "amplify_en" {
                 field_to_match {
                   uri_path {}
                 }
-                regex_string = "^/api/submission/$"
+                regex_string = "^/api/submission(/.*)?$"
                 text_transformation {
                   priority = 0
                   type     = "LOWERCASE"
@@ -102,7 +102,7 @@ resource "aws_wafv2_web_acl" "amplify_en" {
 
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-en-rate-limit"
+      metric_name                = "${var.product_name}-amplify-docs-rate-limit"
       sampled_requests_enabled   = true
     }
   }
@@ -134,12 +134,12 @@ resource "aws_wafv2_web_acl" "amplify_en" {
 
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-en-block-large-requests"
-      sampled_requests_enabled   = false
+      metric_name                = "${var.product_name}-amplify-docs-block-large-requests"
+      sampled_requests_enabled   = true
     }
   }
 
-  # Only allow POST on /api/submission/
+  # Only allow POST on /api/submission
   rule {
     name     = "SubmissionMethodRestriction"
     priority = 4
@@ -152,7 +152,7 @@ resource "aws_wafv2_web_acl" "amplify_en" {
       and_statement {
         statement {
           regex_match_statement {
-            regex_string = "^/api/submission/$"
+            regex_string = "^/api/submission(/.*)?$"
             field_to_match {
               uri_path {}
             }
@@ -168,13 +168,13 @@ resource "aws_wafv2_web_acl" "amplify_en" {
             statement {
               byte_match_statement {
                 positional_constraint = "EXACTLY"
-                search_string         = "POST"
+                search_string         = "post"
                 field_to_match {
                   method {}
                 }
                 text_transformation {
                   priority = 0
-                  type     = "NONE"
+                  type     = "LOWERCASE"
                 }
               }
             }
@@ -185,12 +185,12 @@ resource "aws_wafv2_web_acl" "amplify_en" {
 
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-en-submission-method-restriction"
+      metric_name                = "${var.product_name}-amplify-docs-submission-method-restriction"
       sampled_requests_enabled   = true
     }
   }
 
-  # API Endpoint Protection - Whitelist known good paths (/api/submission/) and block everything else under /api/
+  # API Endpoint Protection - Whitelist known good paths (/api/submission) and block everything else under /api/
   rule {
     name     = "APIEndpointProtection"
     priority = 5
@@ -223,7 +223,7 @@ resource "aws_wafv2_web_acl" "amplify_en" {
           not_statement {
             statement {
               regex_match_statement {
-                regex_string = "^/api/submission/$"
+                regex_string = "^/api/submission(/.*)?$"
                 field_to_match {
                   uri_path {}
                 }
@@ -240,273 +240,14 @@ resource "aws_wafv2_web_acl" "amplify_en" {
 
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "APIEndpointProtection"
+      metric_name                = "${var.product_name}-amplify-docs-api-endpoint-protection"
       sampled_requests_enabled   = true
     }
   }
 
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "${var.product_name}-amplify-en"
-    sampled_requests_enabled   = true
-  }
-
-  tags = {
-    CostCentre = var.billing_code
-    Terraform  = true
-  }
-}
-
-# WAF ACL for Design System Documentation (FR)
-resource "aws_wafv2_web_acl" "amplify_fr" {
-  provider = aws.us-east-1
-  name     = "waf-${var.product_name}-amplify-fr-${var.env}"
-  scope    = "CLOUDFRONT"
-
-  default_action {
-    allow {}
-  }
-
-  # AWS Managed Rules - Common Rule Set
-  rule {
-    name     = "AWSManagedRulesCommonRuleSet"
-    priority = 0
-
-    override_action {
-      none {}
-    }
-
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-fr-common-rules"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  # AWS managed bad input protections (includes Log4j-style payload detection)
-  rule {
-    name     = "AWSManagedRulesKnownBadInputsRuleSet"
-    priority = 1
-
-    override_action {
-      none {}
-    }
-
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesKnownBadInputsRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-fr-known-bad-inputs"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  # Rate Limiting Rule (only POST to /api/submission/)
-  rule {
-    name     = "RateLimitRule"
-    priority = 2
-
-    action {
-      block {}
-    }
-
-    statement {
-      rate_based_statement {
-        limit              = 1000
-        aggregate_key_type = "IP"
-
-        scope_down_statement {
-          and_statement {
-            statement {
-              regex_match_statement {
-                field_to_match {
-                  uri_path {}
-                }
-                regex_string = "^/api/submission/$"
-                text_transformation {
-                  priority = 0
-                  type     = "LOWERCASE"
-                }
-              }
-            }
-
-            statement {
-              regex_match_statement {
-                field_to_match {
-                  method {}
-                }
-                regex_string = "^post$"
-                text_transformation {
-                  priority = 0
-                  type     = "LOWERCASE"
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-fr-rate-limit"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  # Block large requests to prevent abuse and DoS attacks
-  rule {
-    name     = "BlockLargeRequests"
-    priority = 3
-
-    action {
-      block {}
-    }
-
-    statement {
-      size_constraint_statement {
-        field_to_match {
-          body {
-            oversize_handling = "MATCH"
-          }
-        }
-        comparison_operator = "GT"
-        size                = 20480 # 20 KB
-        text_transformation {
-          priority = 0
-          type     = "NONE"
-        }
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-fr-block-large-requests"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  # Only allow POST on /api/submission/
-  rule {
-    name     = "SubmissionMethodRestriction"
-    priority = 4
-
-    action {
-      block {}
-    }
-
-    statement {
-      and_statement {
-        statement {
-          regex_match_statement {
-            regex_string = "^/api/submission/$"
-            field_to_match {
-              uri_path {}
-            }
-            text_transformation {
-              priority = 0
-              type     = "LOWERCASE"
-            }
-          }
-        }
-
-        statement {
-          not_statement {
-            statement {
-              byte_match_statement {
-                positional_constraint = "EXACTLY"
-                search_string         = "POST"
-                field_to_match {
-                  method {}
-                }
-                text_transformation {
-                  priority = 0
-                  type     = "NONE"
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-fr-submission-method-restriction"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  # API Endpoint Protection - Whitelist known good paths (/api/submission/) and block everything else under /api/
-  rule {
-    name     = "APIEndpointProtection"
-    priority = 5
-
-    action {
-      block {
-        custom_response {
-          response_code = 403
-        }
-      }
-    }
-
-    statement {
-      and_statement {
-        statement {
-          byte_match_statement {
-            positional_constraint = "STARTS_WITH"
-            field_to_match {
-              uri_path {}
-            }
-            search_string = "/api/"
-            text_transformation {
-              priority = 0
-              type     = "LOWERCASE"
-            }
-          }
-        }
-
-        statement {
-          not_statement {
-            statement {
-              regex_match_statement {
-                regex_string = "^/api/submission/$"
-                field_to_match {
-                  uri_path {}
-                }
-                text_transformation {
-                  priority = 0
-                  type     = "LOWERCASE"
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "${var.product_name}-amplify-fr-api-protection"
-      sampled_requests_enabled   = true
-    }
-  }
-
-  visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "${var.product_name}-amplify-fr"
+    metric_name                = "${var.product_name}-amplify-docs"
     sampled_requests_enabled   = true
   }
 
@@ -520,13 +261,13 @@ resource "aws_wafv2_web_acl" "amplify_fr" {
 resource "aws_wafv2_web_acl_association" "amplify_en" {
   provider     = aws.us-east-1
   resource_arn = aws_amplify_app.design_system_docs_en.arn
-  web_acl_arn  = aws_wafv2_web_acl.amplify_en.arn
+  web_acl_arn  = aws_wafv2_web_acl.amplify_docs.arn
 }
 
-# Associate WAF ACL with Amplify FR app
+# Associate the same WAF ACL with Amplify FR app
 resource "aws_wafv2_web_acl_association" "amplify_fr" {
   provider     = aws.us-east-1
   resource_arn = aws_amplify_app.design_system_docs_fr.arn
-  web_acl_arn  = aws_wafv2_web_acl.amplify_fr.arn
+  web_acl_arn  = aws_wafv2_web_acl.amplify_docs.arn
 }
 
